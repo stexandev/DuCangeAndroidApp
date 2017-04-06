edbc04a9bf5af6e48088e32d9bfa39ea403d4cfd
stexandev <hynek@gmx.com>
2017-04-06 16:16:27 +0200

    implements query of table rows according to user input as List of Arrays

--- app/src/main/java/de/stexan/ducangeandroidapp/MainActivity.java
+++ app/src/main/java/de/stexan/ducangeandroidapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -16,12 +17,18 @@ public class MainActivity extends AppCompatActivity {
         try {
             DatabaseAccess db = new DatabaseAccess(this);
             /* TESTS
+            //Datenbankname ausgeben
             textBoxView.setText(db.getDatabaseName());
+            //Eintrag nach id ausgeben
             String[] entry = db.accessEntry("A1");
             textBoxView.setText(entry[3]);
-            */
+            //Tabellenzeile als Array of Arrays, mit (maximaler) Länge MAX_RETURNS
             String[][] form = db.entryList("ab");
             textBoxView.setText(form[0][2]);
+            */
+            //alternative Funktion, die Tabellenzeilen als Liste von String-Arrays zurückgibt, Reihenfolge und Anzahl der Zeilen ist laufzeitabhängig!
+            List<String[]> form = db.entryListNew("ab");
+            textBoxView.setText(form.get(0)[2]);
         } catch (IOException e) {
             e.printStackTrace();
         }
