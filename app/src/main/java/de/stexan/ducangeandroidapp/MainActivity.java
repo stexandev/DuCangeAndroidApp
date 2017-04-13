4ee548e6a1b69670dc19cb107fa1ff254c3d2f1f
stexandev <hynek@gmx.com>
2017-04-13 17:36:19 +0200

    added mock-up toolbar and merged TextListener

--- app/src/main/java/de/stexan/ducangeandroidapp/MainActivity.java
+++ app/src/main/java/de/stexan/ducangeandroidapp/MainActivity.java
@@ -4,10 +4,10 @@ import android.app.SearchManager;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -19,11 +19,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.appbar);
-        setSupportActionBar(myToolbar);
-        TextView textBoxView = (TextView)findViewById(R.id.textBox);
+        final Toolbar theToolbar = (Toolbar) findViewById(R.id.appbar);
+        setSupportActionBar(theToolbar);
 
-        SearchView searchView = ( SearchView) findViewById (R.id.searchView);
+        /* TESTS
+        TextView textBoxView = (TextView)findViewById(R.id.textBox);
+        try {
+            DatabaseAccess db = new DatabaseAccess(this);
+
+            //Datenbankname ausgeben
+            textBoxView.setText(db.getDatabaseName());
+            //Eintrag nach id ausgeben
+            String[] entry = db.accessEntry("A1");
+            textBoxView.setText(entry[3]);
+            //Tabellenzeile als Array of Arrays, mit (maximaler) Länge MAX_RETURNS
+            String[][] form = db.entryList("ab");
+            textBoxView.setText(form[0][2]);
+            //alternative Funktion, die Tabellenzeilen als Liste von String-Arrays zurückgibt, Reihenfolge und Anzahl der Zeilen ist laufzeitabhängig!
+            List<String[]> form = db.entryListNew("ab");
+            textBoxView.setText(form.get(0)[2]);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        */
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        // Inflate menu to add items to action bar if it is present.
+        inflater.inflate(R.menu.appbar, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+
+        /* Habe deinen Teil an die Toolbar angeschlossen. TextChange und TextSubmit klappen.
+         * Wäre besser, wenn du diesen Teil in die Seach.java auslagerst und den SearchManager benutzt,
+         * damit die Ergebnisse nicht in einer extra Liste ausgegeben werden müssen, sondern in den
+         * Suchvorschlägen des SearchWidgets. Das Grundgerüst habe ich schon vorgegeben.
+         */
         searchView.setOnQueryTextListener(
                 new SearchView.OnQueryTextListener() {
                     @Override
@@ -31,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
 
                         System.out.println("textSubmit");
 
-                        (( SearchView) findViewById (R.id.searchView)).clearFocus();
+                        ( findViewById (R.id.action_search)).clearFocus();
 
                         (( TextView) findViewById( R.id.searchViewResult ) ).setText("sdfsdf");
 
@@ -50,40 +87,6 @@ public class MainActivity extends AppCompatActivity {
 
                 }
         );
-
-
-
-        try {
-            DatabaseAccess db = new DatabaseAccess(this);
-            /* TESTS
-            //Datenbankname ausgeben
-            textBoxView.setText(db.getDatabaseName());
-            //Eintrag nach id ausgeben
-            String[] entry = db.accessEntry("A1");
-            textBoxView.setText(entry[3]);
-            //Tabellenzeile als Array of Arrays, mit (maximaler) Länge MAX_RETURNS
-            String[][] form = db.entryList("ab");
-            textBoxView.setText(form[0][2]);
-            */
-            //alternative Funktion, die Tabellenzeilen als Liste von String-Arrays zurückgibt, Reihenfolge und Anzahl der Zeilen ist laufzeitabhängig!
-            List<String[]> form = db.entryListNew("ab");
-            textBoxView.setText(form.get(0)[2]);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        // Inflate menu to add items to action bar if it is present.
-        inflater.inflate(R.menu.appbar, menu);
-
-        // Associate searchable configuration with the SearchView
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
 
         return true;
     }
--- app/src/main/java/de/stexan/ducangeandroidapp/Search.java
+++ app/src/main/java/de/stexan/ducangeandroidapp/Search.java
@@ -17,6 +17,14 @@ public class Search extends ListActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /* Ab hier kannst du loslegen :)
+         *
+         */
+
+
+
+
+
 
         // Get the intent, verify the action and get the query
         Intent intent = getIntent();
